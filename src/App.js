8152e66382509aa649af3af3d81ab8192c107f27
Kyle Sullivan <ksullivan@roundsphere.com>
2018-06-02 23:36:08 -0400

    Remove Home component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,16 @@ import { BrowserRouter as Router, Route, NavLink, Link } from 'react-router-dom'
 import OrderWrapper from './orderWrapper'
 import InventoryWrapper from './inventoryWrapper'
 
-class Home extends Component{
-    render(){
-        return (
-            <div>
-                <Link to='/inventory'>Inventory</Link>
-                <Link to="/orders">Orders</Link>
-            </div>
-        )
-    }
-}
 class App extends Component {
     render(){
         return (
             <Router>
                 <div className="App width-wrapper">
                     <ul>
-                        <li><NavLink exact to="/">Home</NavLink></li>
                         <li><NavLink to="/inventory">Inventory</NavLink></li>
                         <li><NavLink to="/orders">Orders</NavLink></li>
                     </ul>
                     <hr />
-                    <Route exact path="/" component={ Home } />
                     <Route path="/inventory" component={ InventoryWrapper } />
                     <Route path="/orders" component={ OrderWrapper } />
                 </div>
