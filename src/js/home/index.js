0b1b10c0a7692b91c52289c2986c5ad60553d66f
Kyle Sullivan <ksullivan@roundsphere.com>
2018-12-21 16:56:37 -0500

    Initial add dashboard to Home

--- src/js/home/index.js
+++ src/js/home/index.js
@@ -1,28 +1,33 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-const Home = () => (
+import Dashboard from "../dashboard"
+
+const Home = props => (
 	<section>
-		<div className="padBot20 flex-parent__center-both">
-			<Link to="/orders/create" className="btn">
-				Create an Order
-			</Link>
+		<div className="flex-parent__wrap flex-parent__center-both">
+			<div className="pad10">
+				<Link to="/orders/create" className="btn">
+					Create an Order
+				</Link>
+			</div>
+			<div className="pad10">
+				<Link to="/orders/incomplete" className="btn">
+					View Incomplete Orders
+				</Link>
+			</div>
+			<div className="pad10">
+				<Link to="/inventory" className="btn">
+					View Inventory
+				</Link>
+			</div>
+			<div className="pad10">
+				<Link to="/inventory/create" className="btn">
+					Add Inventory Item
+				</Link>
+			</div>
 		</div>
-		<div className="padBot20 flex-parent__center-both">
-			<Link to="/orders/incomplete" className="btn">
-				View Incomplete Orders
-			</Link>
-		</div>
-		<div className="padBot20 flex-parent__center-both">
-			<Link to="/inventory" className="btn">
-				View Inventory
-			</Link>
-		</div>
-		<div className="padBot20 flex-parent__center-both">
-			<Link to="/inventory/create" className="btn">
-				Add Inventory Item
-			</Link>
-		</div>
+		<Dashboard />
 	</section>
 )
 
