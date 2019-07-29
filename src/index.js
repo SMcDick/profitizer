6bd0724e824232ab85764c5cd1c7f1e276b21b95
Kyle Sullivan <ksullivan@roundsphere.com>
2019-07-29 17:27:42 -0400

    Update packages and add semantic-ui-react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./scss/style.css"
 import App from "./js/App"
 import { unregister } from "./registerServiceWorker"
+
+import 'semantic-ui-css/semantic.min.css'
+import "./scss/style.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
 unregister()
